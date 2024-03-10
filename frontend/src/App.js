f5d1baa467a1cf906e03420574366ad62337b046
salahAhmed <salahkobia13@gmail.com>
2024-03-10 19:15:58 +0200

    install tailwind and react-icons

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import TopBar from "./components/topBar/topBar";
+function App() {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/topBar/topBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <React.Fragment>
+            TOPbar
+        </React.Fragment>
+    )
+}
+
+export default TopBar
