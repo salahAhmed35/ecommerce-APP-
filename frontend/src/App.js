88599fe3e2f4937a0c4f7c3f77d3f4d9d79bcc80
salahAhmed <salahkobia13@gmail.com>
2024-03-10 21:53:39 +0200

    create top bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { GrAndroid } from "react-icons/gr";
 import TopBar from "./components/topBar/topBar";
 function App() {
   return (
     <>
-     
+     <TopBar />
     </>
   );
 }
--- frontend/src/components/topBar/topBar.jsx
+++ frontend/src/components/topBar/topBar.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import { LuUser2 } from "react-icons/lu";
 const TopBar = () => {
-    return (
-        <React.Fragment>
-            TOPbar
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="top-bar flex  justify-end px-2 py-4 bg-gray-100 border-b border-gray-300 border-solid ">
+        <div className="flex items-center mr-3">
+          <div className="language mr-4">
+            <p className="font-semibold text-gray-600">English</p>
+          </div>
+          <div className="currency mr-4 font-semibold text-gray-600">$ US Dollar</div>
+        </div>
+        <div className="flex items-center ml-4">
+          <div className="register mr-3">
+            <a href="#" className="font-semibold text-gray-600 flex items-center">
+              <LuUser2 className="mr-1" />
+              <p>register</p>
+            </a>
+          </div>
+          <span className="text-gray-600 font-semibold">|</span>
+          <div className="login mx-3">
+            <a href="#" className="font-semibold text-gray-600">login</a>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default TopBar
+export default TopBar;
