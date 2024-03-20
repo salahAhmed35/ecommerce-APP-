586ea1530632968882c23ffc12bbed9dcd096907
salahAhmed <salahkobia13@gmail.com>
2024-03-21 01:25:45 +0200

    create user login route api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -24,4 +24,20 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ message: 'Server Error' });
     }
 });
+router.post('/login' , async (req , res) => {
+    const {email , password} = req.body;
+    try {
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message : 'user not found'})
+        }
+        const passwordMatched = bcrypt.compare(password , user.password)
+        if(!passwordMatched){
+            return res.status(400).json({message : 'password not matched'})
+        }
+        if(passwordMatched){
+            return res.status(201).json({message : "successfull login"})
+        }
+    }catch(err){}
+})
 module.exports = router
