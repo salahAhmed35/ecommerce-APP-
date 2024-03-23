42941bd9e76a8ec9f0db0c211b751ee19e683800
salahAhmed <salahkobia13@gmail.com>
2024-03-23 18:03:32 +0200

    create product schema model

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,6 +7,15 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }
 });
 
+const productSchema = new mongoose.Schema({
+    name: {type : String ,require: true},
+    description: {type : String ,require: true},
+    category: {type : String ,require: true},
+    image: [String],
+    quantity: {type : Number ,require: true},
+    rating: {type : Number ,require: true},
+    price: {type : Number ,require: true}, 
+})
 const User = mongoose.model('User' , userSchema)
 
 module.exports = User
